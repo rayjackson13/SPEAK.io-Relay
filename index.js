4a237292b67716f51a8eeefef4a23ad81f3f14c5
rayjackson13 <rayjackson@live.ru>
2022-01-30 05:14:41 +0600

    Added a status html page.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+const path = require('path');
 const express = require('express');
 const gun = require('gun');
 const app = express();
 const port = 3030;
 app.use(gun.serve);
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
 
 const server = app.listen(port, () => {
   console.info(`✔ App is running on port ${port}.`)
